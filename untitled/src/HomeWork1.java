db8e6d5eefc11c31afc9a8fdce1c27bd8b5780cf
tburkitov <timur.burkitov@gmail.com>
2023-09-12 10:17:13 +0300

    Домшняя работа 2,добавление всяких файлов

--- untitled/src/HomeWork1.java
+++
@@ -1,9 +0,0 @@
-public class HomeWork1 {
-     public static void main(String[] args) {
-          Point p1 = new Point(7,3);
-          Point p2 = new Point(15, 12);
-          System.out.println("Расстояние между точками а (" + p1.x +"; " + p1.y+ ") и  b (" + p2.x + "; " + p2.y + ") = " + p1.distance(p2));
-
-
-     }
-}
