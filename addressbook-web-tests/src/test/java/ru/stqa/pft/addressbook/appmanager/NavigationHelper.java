f4e6e5ed6d3514056570ce881f62bc7074d44489
tburkitov <timur.burkitov@gmail.com>
2023-10-07 15:07:18 +0300

    Домшняя работа 3, м-9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -5,21 +5,24 @@ import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase {
 
-    private WebDriver wd;
-
     public NavigationHelper(WebDriver wd) {
         super(wd);
     }
 
 
     public void gotoGroupPage() {
-        click(By.linkText("groups"));
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        } click(By.linkText("groups"));
     }
 
     public void gotoContactPage() {
       click(By.linkText("add new"));
     }
 
-    public void gotoHome() {
+    public void gotoHomePage() {
+        click(By.linkText("home"));
     }
 }
